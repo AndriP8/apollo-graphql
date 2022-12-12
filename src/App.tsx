51c6a4e3ac5b9430815bb109aa9b4177c6f0c091
AndriPurnomo <andri.adrp@gmail.com>
2022-12-12 23:25:40 +0700

    initial graphql implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+import { Box, Heading, Image, Spinner, Text } from "@chakra-ui/react";
+
+interface Data {
+  locations: {
+    __typename: string;
+    id: string;
+    name: string;
+    description: string;
+    photo: string;
+  }[];
+}
 
 function App() {
+  const GET_LOCATIONS = gql`
+    query GetLocations {
+      locations {
+        id
+        name
+        description
+        photo
+      }
+    }
+  `;
+
+  const { data, loading, error } = useQuery<Data>(GET_LOCATIONS);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box background="gray.800" minH="100vh" height="full" color="white">
+      {loading && <Spinner />}
+      {error && <Text>{error.message}</Text>}
+      {data &&
+        data?.locations.map((location) => {
+          return (
+            <Box key={location.id}>
+              <Heading>{location.name}</Heading>
+              <Image src={location.photo} />
+              <Text>About this location</Text>
+              <Text>{location.description}</Text>
+            </Box>
+          );
+        })}
+    </Box>
   );
 }
 
